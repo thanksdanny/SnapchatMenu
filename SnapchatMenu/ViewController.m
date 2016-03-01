cb7348c6ebcf3e904a5d7df479cde6bfc5cb5d5f
thanksdanny <hj-07@163.com>
2016-03-01 11:16:08 +0800

    实现左右翻

--- SnapchatMenu/ViewController.m
+++ SnapchatMenu/ViewController.m
@@ -24,20 +24,28 @@
     
     [UIApplication sharedApplication].statusBarHidden = YES; // 貌似无效
     LeftView *leftView = [[LeftView alloc] initWithNibName:@"LeftView" bundle:nil];
-    CameraView *cameraView = [[CameraView alloc] initWithNibName:@"CameraView" bundle:nil];
+    CameraView *centerView = [[CameraView alloc] initWithNibName:@"CameraView" bundle:nil];
     RightView *rightView = [[RightView alloc] initWithNibName:@"RightView" bundle:nil];
     
     [self addChildViewController:leftView];
     [self.scrollView addSubview:leftView.view];
     [leftView didMoveToParentViewController:self];
     
-    [self addChildViewController:cameraView];
-    [self.scrollView addSubview:cameraView.view];
-    [cameraView didMoveToParentViewController:self];
+    [self addChildViewController:centerView];
+    [self.scrollView addSubview:centerView.view];
+    [centerView didMoveToParentViewController:self];
     
     [self addChildViewController:rightView];
-    [self.scrollView addSubview:cameraView.view];
-    [cameraView didMoveToParentViewController:self];
+    [self.scrollView addSubview:rightView.view];
+    [rightView didMoveToParentViewController:self];
+    
+    CGRect centerViewFrame = centerView.view.frame;
+    centerViewFrame.origin.x = self.view.frame.size.width;
+    centerView.view.frame = centerViewFrame;
+    
+    CGRect rightViewFrame = rightView.view.frame;
+    rightViewFrame.origin.x = 2 * self.view.frame.size.width;
+    rightView.view.frame = rightViewFrame;
     
     self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width * 3, self.view.frame.size.height);
 }
